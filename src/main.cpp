ea96eb0111ac0442d2c86c598c2ac55feee2416e
adilahrosly05-usm <adilahrosly05@student.usm.my>
2025-12-10 13:33:47 +0800

    done

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,8 @@
 #include <iostream>
 using namespace std;
 
-// Declare functions (need to be supdate)
-void showMenu();
-void addVehicle();
-void showAll();
-void searchByPlate();
-void saveToFiles();
-void loadFromFiles();
-
-int main() {
-    int choice;
-    do {
-        showMenu();
-        cin >> choice;
-        switch (choice) {
-            case 1:
-                addVehicle();
-                break;
-            case 2:
-                showAll();
-                break;
-            case 3:
-                searchByPlate();
-                break;
-            case 4:
-                saveToFiles();
-                break;
-            case 5:
-                loadFromFiles();
-                break;
-            case 6:
-                cout << "Exiting program." << endl;
-                break;
-            default:
-                cout << "Invalid choice. Please try again." << endl;
-        }
-    } while (choice != 6);
+int main() 
+{
+    cout << "PARKING SYSYTEM" << endl;
     return 0;
 }
