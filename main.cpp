9ada90d0c2c3e4359b89c9bfe0e41a12d8c3fd5e
adilahrosly05-usm <adilahrosly05@student.usm.my>
2025-12-24 02:12:34 +0800

    main

--- main.cpp
+++ main.cpp
@@ -9,3 +9,30 @@
 
 using namespace std;
 
+int main() {
+    string vehicleType, plateNumber;
+    int hours;
+    double fee;
+
+    cout << "===== PARKING BILL SYSTEM =====" << endl;
+
+    vehicleType = inputVehicleType();
+    if (!validateVehicleType(vehicleType)) {
+        cout << "Invalid vehicle type!" << endl;
+        return 0;
+    }
+
+    plateNumber = inputPlateNumber();
+    hours = inputHours();
+
+    if (!validateHours(hours)) {
+        cout << "Invalid hours!" << endl;
+        return 0;
+    }
+
+    fee = calculateFee(vehicleType, hours);
+    displayReceipt(vehicleType, plateNumber, hours, fee);
+    saveToFile(vehicleType, plateNumber, hours, fee);
+
+    return 0;
+}
